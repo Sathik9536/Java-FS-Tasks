661077eca5fc41f990ca3dd949f9ab9905bbbad4
Ahamed Sathik <156398402+Sathik9536@users.noreply.github.com>
2024-08-08 10:34:43 +0530

    Add files via upload

---
+++ August 06-08-2024/Employee.java
@@ -0,0 +1,24 @@
+package pack.programs;
+class pro
+{
+	int emp_id;
+	String emp_name;
+	void insert(int n, String name)
+	{
+		emp_id=n;
+		emp_name=name;
+	}
+	void display()
+	{
+		System.out.println(emp_id+" "+emp_name);
+	}
+}
+public class Employee 
+{
+	public static void main(String[] args) 
+	{
+		pro s=new pro();
+		s.insert(101, "Sathik");
+		s.display();
+	}
+}
---
+++ August 06-08-2024/calculator.java
@@ -0,0 +1,16 @@
+package pack.programs;
+
+public class calculator {
+	public static int add(int a, int b)
+	{
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		
+		int result = add(5,7);
+			System.out.println(result);
+		
+	}
+
+}
