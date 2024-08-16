a9b8c011bc277d5eadf01ec21c39ff23065e29d9
Ahamed Sathik <156398402+Sathik9536@users.noreply.github.com>
2024-08-16 11:04:42 +0530

    Add files via upload

---
+++ August 14-08-2024/Wrap.java
@@ -0,0 +1,20 @@
+package mine;
+
+public class Wrap {
+
+	public static void main(String[] args) {
+		int a = 20;
+		boolean b = true;
+		
+		Integer i = Integer.valueOf(a);; // Boxing, wrapping
+		
+		boolean j = Boolean.valueOf(b);
+		
+		System.out.println(i+" "+b);
+		
+		
+
+		
+	}
+
+}
---
+++ August 14-08-2024/unboxing.java
@@ -0,0 +1,14 @@
+package mine;
+
+public class unboxing {
+
+	public static void main(String[] args) {
+		int a = 10;
+	
+		Integer i = Integer.valueOf(a);
+		
+		int j = i.intValue(); // UnBoxing
+
+	}
+
+}
